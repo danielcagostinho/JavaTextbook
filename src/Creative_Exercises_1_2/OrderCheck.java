0feb82c616b41f2c8b1dca74cc4f84bb06814beb
Daniel Agostinho <danielcagostinho@gmail.com>
2019-03-21 12:33:07 -0400

    finished up until exercise 1.2.29 DayofWeek

---
+++ src/Creative_Exercises_1_2/OrderCheck.java
@@ -0,0 +1,12 @@
+package Creative_Exercises_1_2;
+
+public class OrderCheck {
+
+    public static void main(String[] args){
+        // Exercise 1.2.28 - This program takes 3 doubles from the command line and returns true if they are strictly increasing or decreasing
+        double x = Double.parseDouble(args[0]);
+        double y = Double.parseDouble(args[1]);
+        double z = Double.parseDouble(args[2]);
+        System.out.println((x<y && y<z)|| (x>y && y>z));
+    }
+}
