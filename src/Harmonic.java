592da377d1c7f055c33a4435fc4c4af80b32a765
Daniel Agostinho <danielcagostinho@gmail.com>
2019-05-02 10:21:57 -0400

    finished all lessons in 1.3

---
+++ src/Harmonic.java
@@ -0,0 +1,14 @@
+public class Harmonic {
+
+    public static void main(String[] args){
+        // Computes the Nth Harmonic number.
+        int N = Integer.parseInt(args[0]);
+        double sum = 0.0;
+        for (int i = 1; i <= N; i++){
+            sum += 1.0/i;
+        }
+        System.out.println(sum);
+    }
+
+
+}
